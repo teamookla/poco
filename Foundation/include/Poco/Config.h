8b479ba3d4c69f90234cb936bed8312d3a27149e
Matt Tucker <tuck@whistlingfish.net>
2016-03-11 11:19:08 -0800

    Ookla-specific build changes
    * Support for Jenkins
    * External openssl

--- Foundation/include/Poco/Config.h
+++ Foundation/include/Poco/Config.h
@@ -185,6 +185,7 @@
 //   empty or other value:
 //     Do not link any OpenSSL libraries automatically. You will have to edit the
 //     Visual C++ project files for Crypto and NetSSL_OpenSSL.
+#define POCO_EXTERNAL_OPENSSL
 #ifndef POCO_EXTERNAL_OPENSSL
 	#define POCO_EXTERNAL_OPENSSL POCO_EXTERNAL_OPENSSL_SLPRO
 #endif
