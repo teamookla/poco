cece8fa0fc6abafc58cf7c92c11214ff48d5d1c1
Matt Tucker <tuck@whistlingfish.net>
2018-08-29 16:05:20 -0700

    jenkins2: architecture build fixes
    * use custom workspace including build name to separate 32/64-bit builds
    * use cmake VS generator including architecture in Windows to build proper architecture
    * build Win64 build as 32-bit for now (OpenSSL linking issues)

--- jenkins.cmd
+++ jenkins.cmd
@@ -19,26 +19,26 @@ if not "%VSINSTALLDIR%"=="" goto vs_install_dir_defined
 if not defined VS_VERSION (
   set VS_VERSION=vs140
 )
-if %VS_VERSION%==vs90 (
+if "%VS_VERSION%"=="vs90" (
   set VS_VERSION_NUMBER=90
   set VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 2008"
-) else if %VS_VERSION%==vs100 (
+) else if "%VS_VERSION%"=="vs100" (
   set VS_VERSION_NUMBER=100
   set VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 2010"
-) else if %VS_VERSION%==vs110 (
+) else if "%VS_VERSION%"=="vs110" (
   set VS_VERSION_NUMBER=110
   set VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 2012"
-) else if %VS_VERSION%==vs120 (
+) else if "%VS_VERSION%"=="vs120" (
   set VS_VERSION_NUMBER=120
   set VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 2013"
-) else if %VS_VERSION%==vs140 (
+) else if "%VS_VERSION%"=="vs140" (
   set VS_VERSION_NUMBER=140
   if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\" (
     set VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 14.0"
   ) else (
     set VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 2015"
   )
-) else if %VS_VERSION%==vs150 (
+) else if "%VS_VERSION%"=="vs150" (
   set VS_VERSION_NUMBER=150
   set VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 2017"
 )
@@ -56,6 +56,41 @@ call %VSINSTALLDIR%\VC\vcvarsall.bat %platform%
 
 :vs_vars_loaded
 
+rem Set CMake Generator
+rem
+rem Visual Studio 15 2017 [arch] = Generates Visual Studio 2017 project files.
+rem                                Optional [arch] can be "Win64" or "ARM".
+rem Visual Studio 14 2015 [arch] = Generates Visual Studio 2015 project files.
+rem                                Optional [arch] can be "Win64" or "ARM".
+rem Visual Studio 12 2013 [arch] = Generates Visual Studio 2013 project files.
+rem                                Optional [arch] can be "Win64" or "ARM".
+rem Visual Studio 11 2012 [arch] = Generates Visual Studio 2012 project files.
+rem                                Optional [arch] can be "Win64" or "ARM".
+rem Visual Studio 10 2010 [arch] = Generates Visual Studio 2010 project files.
+rem                                Optional [arch] can be "Win64" or "IA64".
+rem Visual Studio 9 2008 [arch]  = Generates Visual Studio 2008 project files.
+rem                                Optional [arch] can be "Win64" or "IA64".
+rem
+if "%VS_VERSION%"=="vs90" (
+  set CMAKE_GENERATOR=Visual Studio 9 2008
+) else if "%VS_VERSION%"=="vs100" (
+  set CMAKE_GENERATOR=Visual Studio 10 2010
+) else if "%VS_VERSION%"=="vs110" (
+  set CMAKE_GENERATOR=Visual Studio 11 2012
+) else if "%VS_VERSION%"=="vs120" (
+  set CMAKE_GENERATOR=Visual Studio 12 2013
+) else if "%VS_VERSION%"=="vs140" (
+  set CMAKE_GENERATOR=Visual Studio 14 2015
+) else if "%VS_VERSION%"=="vs150" (
+  set CMAKE_GENERATOR=Visual Studio 15 2017
+) else (
+  echo Error: VS_VERSION not set
+  exit /b 1
+)
+if "%platform%" == "x64" (
+  set CMAKE_GENERATOR=%CMAKE_GENERATOR% Win64
+)
+
 rem Build Poco
 set POCO_BASE=%cd%
 
@@ -65,6 +100,7 @@ mkdir cmake_build_Release
 cd cmake_build_Release
 
 cmake .. ^
+  -G "%CMAKE_GENERATOR%" ^
 	-DENABLE_PDF=OFF ^
 	-DENABLE_MONGODB=OFF ^
 	-DENABLE_DATA=OFF ^
@@ -84,6 +120,7 @@ mkdir cmake_build_Debug
 cd cmake_build_Debug
 
 cmake .. ^
+  -G "%CMAKE_GENERATOR%" ^
 	-DENABLE_PDF=OFF ^
 	-DENABLE_MONGODB=OFF ^
 	-DENABLE_DATA=OFF ^
